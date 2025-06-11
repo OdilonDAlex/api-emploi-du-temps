21c0196ae7384082ec2278d1066d41c2162c4045
Odilon D'Alex <odilondalex2600@gmail.com>
2025-06-11 08:24:40 +0300

    ajout d'un origin  ( localhost:5173 ) dans cors_allowed_origins

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [ '/^https?:\/\/(localhost|127\.0\.0\.1):[0-9]{2,4}/'],
 
